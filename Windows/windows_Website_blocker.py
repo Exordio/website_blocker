e57e195d1a6650719d6c1344a036bca133149847
Exord <wecatorz@gmail.com>
2020-02-17 21:57:25 +0300

    first skeleton

---
+++ Windows/windows_Website_blocker.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import time
+from datetime import datetime as dt
+
+
+hosts = r"C:\Windows\System32\drivers\etc\hosts"
+
+redirect = "127.0.0.1"
+
+website_list = ["www.vk.com", "www.google.com"]
+
