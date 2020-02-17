ea23b5b3c59489042d3ff383e9bd06b2df45888c
Exord <wecatorz@gmail.com>
2020-02-17 22:30:36 +0300

    complete for win

--- Windows/windows_Website_blocker.py
+++ Windows/windows_Website_blocker.py
@@ -3,16 +3,14 @@
 import time
 from datetime import datetime as dt
 
-
-hosts_temp = "hosts"
-hosts = r"C:\Windows\System32\drivers\etc\hosts"
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
-website_list = ["www.vk.com", "www.google.com"]
+website_list = ["www.google.com", "www.vk.com"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 0) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23):
         print("Work Time...")
-        with open(hosts_temp, "r+") as file:
+        with open(hosts_path, "r+") as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -20,13 +18,13 @@ while True:
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
-        with open(hosts_temp, "r+") as file:
+        with open(hosts_path, "r+") as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
             file.truncate()
-        print("Relax time...")
+        print("f")
     
     time.sleep(5)
